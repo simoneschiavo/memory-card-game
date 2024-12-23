7beac9bd447f4b385a6ae3a641d9c6217b672582
Simone Schiavo <simoneschiavo@icloud.com>
2024-12-23 14:10:57 +0100

    feat(App): add general structure

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,37 @@ import ButtonSelector from "./components/ButtonSelector";
 function App() {
   const [level, setLevel] = useState(null);
   const [isOpen, setIsOpen] = useState(true);
+  const [score, setScore] = useState(0);
+  const [record, setRecord] = useState(0);
 
   const handleLevel = (e) => {
     setLevel(e.target.value.toLowerCase());
     setIsOpen(false);
   };
 
+  const increaseScore = () => {
+    setScore(score + 1);
+  };
+
+  const resetScore = () => {
+    setScore(0);
+  };
+
+  const handleRecord = (newScore) => {
+    if (newScore > record) {
+      setRecord(newScore);
+    }
+  };
+
   return (
     <>
+      <Header
+        increaseScore={increaseScore}
+        resetScore={resetScore}
+        handleRecord={handleRecord}
+        score={score}
+        record={record}
+      />
       <section className={isOpen ? "level-selector" : "level-selector close"}>
         <h1>Which level do you want to choose?</h1>
         <div className="row-container levels">
@@ -26,6 +49,7 @@ function App() {
           />
         </div>
       </section>
+      {level === "easy" && <MemoryGame />}
     </>
   );
 }
