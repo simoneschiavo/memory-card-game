01badbf23bdc15f38d5f7f4a9dca5c7e41b79b80
Simone Schiavo <simoneschiavo@icloud.com>
2024-12-23 20:48:52 +0100

    feat: add header css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
+import { PawPrint } from "lucide-react";
+
 export default function Header({ score, record }) {
   return (
     <>
       <header className="navbar">
-        <h1 className="title">MemoryDoggo</h1>
+        <div className="title-container">
+          <h1 className="title">MemoryDoggo</h1>
+          <PawPrint className="paw-print" />
+        </div>
         <div className="scores-container">
           <p className="score">Current score: {score}</p>
           <p className="record">Your record: {record}</p>
